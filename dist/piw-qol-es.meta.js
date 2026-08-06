69767c76e7c20010a49c6d47d03d2f8e4a269fc2
LosaJR <ivaanciio@gmail.com>
2026-08-06 20:42:27 +0200

    Sustituir PIW-QOL por 9.10.24 y activar autoactualización

--- dist/piw-qol-es.meta.js
+++ dist/piw-qol-es.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Poke Idle World - Quality of Life (PIW-QOL ES)
 // @namespace    http://tampermonkey.net/
-// @version      9.10.18
+// @version      9.10.24
 // @description  Mejoras de calidad de vida en español, sin modificar el mapa y con candados de venta configurables.
 // @author       Desjunior (JulianoCLI)
 // @match        https://poke.idleworld.online/play
 // @grant        none
 // @run-at       document-start
-// @updateURL     https://losajr.github.io/PokeIdleWorld-Scripts/piw-qol-es.meta.js
-// @downloadURL   https://losajr.github.io/PokeIdleWorld-Scripts/piw-qol-es.user.js
+// @require      https://cdn.jsdelivr.net/npm/pako@2.1.0/dist/pako.min.js
+// @updateURL    https://raw.githubusercontent.com/LosaJR/PokeIdleWorld-Scripts/main/dist/piw-qol-es.meta.js
+// @downloadURL  https://raw.githubusercontent.com/LosaJR/PokeIdleWorld-Scripts/main/dist/piw-qol-es.user.js
 // ==/UserScript==
